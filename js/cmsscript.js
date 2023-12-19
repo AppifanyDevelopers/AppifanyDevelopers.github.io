6a135a2f3c65a51b070d71fdc858cb5819f62471
AppifanyDevelopers <developers@appifany.com.au>
2023-12-19 07:08:48 +0530

    first commit

--- js/cmsscript.js
+++ js/cmsscript.js
@@ -51,3 +51,13 @@ function sortTable(columnIndex) {
       tbody.appendChild(row);
     });
   }
+
+  function toggleDiv() {
+    var div = document.getElementById("btn-gray");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+  
